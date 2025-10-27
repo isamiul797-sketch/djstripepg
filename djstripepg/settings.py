dc2aeda1b496a176036c998eba6a276744a04799
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-27 18:02:50 +0600

    new files

--- djstripepg/settings.py
+++ djstripepg/settings.py
@@ -109,3 +109,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from decouple import config
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
